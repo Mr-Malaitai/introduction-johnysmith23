e3404746fa7412d2a67f9d8b5e70ccb9cb6510e0
johnyamith23 <sione_kai@student.asdah.school.nz>
2022-02-21 13:33:03 +1300

    added button

---
+++ mian.py
@@ -0,0 +1,12 @@
+# print("Hellow, world")
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+
+submit_button = ttk.Button(root, text = "submit")
+submit_button.grid(row = 0, column = 0 )
+
+
+root.mainloop()
